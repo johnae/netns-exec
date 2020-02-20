848636c0f5c1c99bc5f4922254f89c2e3ca6d473
John Axel Eriksson <john@insane.se>
2020-02-20 08:28:28 +0100

    Fix security issue when dropping privs plus improve error handling a bit

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,23 @@ use std::env;
 use std::ffi::CString;
 
 fn main() {
+    if env::args().len() < 3 {
+        panic!("Please supply at least 2 arguments - the network namespace then the command (and any arguments to that command)");
+    }
+
     let nsname = env::args().into_iter().skip(1).next().unwrap();
+    unshare(CloneFlags::CLONE_NEWNET).expect("Failed to unshare network namespace");
+
     let nspath = format!("/var/run/netns/{}", nsname);
-
-    unshare(CloneFlags::CLONE_NEWNET).expect("failed");
-
     let nsfd = open(nspath.as_str(), OFlag::O_RDONLY, Mode::empty())
         .expect(&format!("Could not open netns file: {}", nspath));
 
-    setns(nsfd, CloneFlags::CLONE_NEWNET).unwrap();
-    // drop privs now
-    setuid(Uid::current()).unwrap();
-    setgid(Gid::current()).unwrap();
+    setns(nsfd, CloneFlags::CLONE_NEWNET).expect("Couldn't set network namespace");
+    // drop privs now - these MUST happen in the below order, otherwise
+    // dropping group privileges might fail as the user privs may have
+    // changed so that the user can no longer set the gid
+    setgid(Gid::current()).expect("Couldn't drop group privileges");
+    setuid(Uid::current()).expect("Couldn't drop user privileges");
 
     let args: Vec<_> = env::args()
         .into_iter()
